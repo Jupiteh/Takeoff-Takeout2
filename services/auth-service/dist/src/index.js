8ec632ac9e3c9fabb102ec637690967b67494352
Julien Lasserre <93577560+julienlasserre@users.noreply.github.com>
2024-06-20 20:44:29 +0200

    4

---
+++ services/auth-service/dist/src/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const config_1 = require("./config");
+const app_1 = __importDefault(require("./app"));
+const PORT = process.env.PORT || 3000;
+const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield (0, config_1.connectDB)();
+        app_1.default.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    }
+    catch (err) {
+        if (err instanceof Error) {
+            console.error('Error starting server:', err.message);
+        }
+        else {
+            console.error('Unexpected error', err);
+        }
+    }
+});
+startServer();
